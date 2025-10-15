2e3b05336dd6f85b05402f036f9ec86e4c4d53ca
Jejomar Parrilla <jpar1252003@gmail.com>
2025-10-15 16:51:56 +0800

    feat(home): Created the solutions section

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import HeroSection from '../components/Home/HeroSection';
+import SolutionsSection from '../components/Home/SolutionsSection';
 
 export default function Home() {
     return (
         <div>
             <HeroSection />
+            <SolutionsSection />
         </div>
     );
 }
